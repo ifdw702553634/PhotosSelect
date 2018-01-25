66ce3efb40c7503891f46a852c92c49843bdcf1d
mude <ifdw702553634@163.com>
2018-01-25 11:56:56 +0800

    create

---
+++ PhotoSelect/PhotoSelect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoSelect
+//
+//  Created by mude on 2018/1/25.
+//  Copyright © 2018年 mude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoSelect/PhotoSelect/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoSelect
+//
+//  Created by mude on 2018/1/25.
+//  Copyright © 2018年 mude. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
